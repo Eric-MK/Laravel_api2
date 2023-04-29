667283ee262dadb9742ea34083447cb9473844b9
unknown <mutungaeric839@gmail.com>
2023-04-29 21:03:09 +0300

    more comments

--- app/Http/Controllers/DeviceController.php
+++ app/Http/Controllers/DeviceController.php
@@ -9,6 +9,6 @@ class DeviceController extends Controller
 {
     function list($id=null)//if we are not getting any id it should be null by default
     {
-        return $id?Device::find($id):Device::all();
+        return $id?Device::find($id):Device::all();//if id is not null it finds the id but if null it finds all
     }
 }
